17362dbbbe6bb990162e153af6d0edca41af02df
unknown <haris.bashir@biantedigital.com>
2024-07-31 20:15:49 +0500

    bloc listener implementation

--- lib/counter/pages/counter_bloc_page.dart
+++ lib/counter/pages/counter_bloc_page.dart
@@ -8,6 +8,7 @@ class CounterBlocPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color textColor = Colors.black;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,59 +16,69 @@ class CounterBlocPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocConsumer<CounterCubit, CounterState>(
-                buildWhen: (previous, current) {
-              return current.count >= 0;
-            }, listenWhen: (previous, current) {
-              return previous.count > 0;
-            }, listener: (BuildContext context, CounterState state) {
-              if (state.count == 0) {
-                final snackBar = SnackBar(
-                  content: const Text('Counter reset to zero'),
-                  action: SnackBarAction(
-                    label: 'Dismiss',
-                    onPressed: () {
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                    },
+        child: BlocListener<CounterCubit, CounterState>(
+          listener: (context, state) {
+            if (state.count == 0) {
+              textColor = Colors.red;
+            } else {
+              textColor = Colors.yellow;
+            }
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocConsumer<CounterCubit, CounterState>(
+                  buildWhen: (previous, current) {
+                return current.count >= 0;
+              }, listenWhen: (previous, current) {
+                return previous.count > 0;
+              }, listener: (BuildContext context, CounterState state) {
+                if (state.count == 0) {
+                  final snackBar = SnackBar(
+                    content: const Text('Counter reset to zero'),
+                    action: SnackBarAction(
+                      label: 'Dismiss',
+                      onPressed: () {
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      },
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              }, builder: (BuildContext context, CounterState state) {
+                print("Build");
+                return Text(
+                  '${state.count}',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: textColor,
                   ),
                 );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }
-            }, builder: (BuildContext context, CounterState state) {
-              print("Build");
-              return Text(
-                '${state.count}',
-                style: const TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                ),
-              );
-            }),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      context.read<CounterCubit>().increment();
-                    },
-                    child: const Text("Increment"),
+              }),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        context.read<CounterCubit>().increment();
+                      },
+                      child: const Text("Increment"),
+                    ),
                   ),
-                ),
-                const SizedBox(width: 30),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      context.read<CounterCubit>().decrement();
-                    },
-                    child: const Text("Decrement"),
+                  const SizedBox(width: 30),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        context.read<CounterCubit>().decrement();
+                      },
+                      child: const Text("Decrement"),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
